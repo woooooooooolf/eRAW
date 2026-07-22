978af153134b17cd4898b11294f5d686aa9516b1
eRAW Development <eraw@localhost>
2026-07-23 00:33:59 +0800

    发布开发版本V0.0.4

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ import type {
 } from "./types";
 import { DEFAULT_DESCRIPTOR } from "./types";
 
-const VERSION = "0.0.3";
+const VERSION = "0.0.4";
 const STORAGE_KEY = "eraw.rawDescriptor.v1";
 const SETTINGS_KEY = "eraw.appSettings.v1";
 
