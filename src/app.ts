6d3317fc74fcc61436b664503565f6f9da8ab97c
eRAW Development <eraw@localhost>
2026-08-25 17:03:27 +0800

    发布：准备 V0.6.0

--- src/app.ts
+++ src/app.ts
@@ -111,7 +111,7 @@ import {
   isQuadCfa,
 } from "./types";
 
-const VERSION = "0.5.5";
+const VERSION = "0.6.0";
 const BUILD_TIME_SOURCE = __ERAW_BUILD_TIME__;
 const STORAGE_KEY = "eraw.rawDescriptor.v1";
 const SETTINGS_KEY = "eraw.appSettings.v1";
